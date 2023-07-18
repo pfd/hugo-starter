9f423e61793b19e89b06d25fbacbde7e2cd3981e
pfd <pdavis@rudderstack.com>
2023-07-18 11:09:41 -0700

    remove algolia

--- vercel.sh
+++ vercel.sh
@@ -3,9 +3,9 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 
 if [[ $VERCEL_ENV == "production"  ]] ; then 
-  amazon-linux-extras install golang1.11 && hugo --gc --minify --ignoreCache --verbose && npm run algolia
+  amazon-linux-extras install golang1.11 && hugo --gc --minify --ignoreCache --verbose
 else 
-  amazon-linux-extras install golang1.11 && hugo -e="staging" --gc --minify --ignoreCache --verbose && npm run algolia
+  amazon-linux-extras install golang1.11 && hugo -e="staging" --gc --minify --ignoreCache --verbose
 fi
 
 
